e8ebc747130e62381617211d18d3345c355d1e23
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2025-01-01 14:12:54 +0300

    Iterative Binary Search

--- BinarySearch/IterativeBinarySearch.java
+++ BinarySearch/IterativeBinarySearch.java
@@ -1,5 +1,46 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
+// input must be sorted
+// or
+// we have to sort array firstly
+
+// https://www.geeksforgeeks.org/binary-search-in-java/
+
+
 public class IterativeBinarySearch {
-    
+    public static void main(String[] args) {
+        System.out.println(iterativeBinarySearch(new int[] {}, 3));
+    }
+
+    public static int iterativeBinarySearch(int[] arr, int target){
+        int result;
+        sort(arr);
+
+        int rIndex = arr.length-1;
+        int lIndex = 0;
+        int midIndex = rIndex+lIndex / 2;
+        
+        while (true) {
+            if (arr[midIndex] == target) {result = midIndex; break;}
+            else if (target>arr[midIndex]){
+                lIndex=midIndex+1;
+                midIndex = rIndex+lIndex / 2;
+                continue;
+            }
+            else if (target<arr[midIndex]){
+                rIndex=midIndex-1;
+                midIndex = rIndex+lIndex / 2;
+                continue;
+            }
+        }
+
+        return result;
+    }
+
+    public static int[] sort(int[] arr){
+        Arrays.sort(arr);
+        return arr;
+    }
 }
---
+++ BinarySearch/readme.md
@@ -0,0 +1,4 @@
+Binary search is one of the searching techniques applied when the input is sorted here we are focusing on finding the middle element that acts as a reference frame whether to go left or right to it as the elements are already sorted. This searching helps in optimizing the search technique with every iteration is referred to as binary search and readers do stress over it as it is indirectly applied in solving questions.
+
+
+![binary-search](https://media.geeksforgeeks.org/wp-content/uploads/20230711134722/Binary-Search.png)
