726f00c9e069295009d1b6efffca3db859ecd5b9
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2025-02-17 06:21:16 +0300

    Array Average

--- Main.java
+++ Main.java
@@ -9,35 +9,15 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MathChallenge(36,54));
+        int[] numbers = {10,10,10,10};
+
+        int total_length = numbers.length;
+        int sum = 0;
+
+        for (int i : numbers) {
+            sum = sum + i;
+        }
+
+        System.out.println("Average: " + (double) sum/total_length);
     }
-
-    public static int MathChallenge(int num1, int num2) {
-        // code goes here
-        HashSet<Integer> num1Remainders = new HashSet<>();
-        ArrayList<Integer> num2Remainders = new ArrayList<>();
-
-        for (int i = 1; i <= num1; i++) {
-            if (num1%i==0) {
-                num1Remainders.add(i);
-            }
-        }
-
-        for (int i = 1; i <= num2; i++) {
-            if (num2%i==0) {
-                num2Remainders.add(i);
-            }
-        }
-
-        int bigCommon = 0;
-        for (int i = num2Remainders.size()-1; i >=0; i--) {
-            if (num1Remainders.contains(num2Remainders.get(i))) {
-                bigCommon = num2Remainders.get(i);
-                break;
-            }
-        }
-
-        return bigCommon;
-      }
-
 }
